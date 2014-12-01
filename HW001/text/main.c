20b8d908a7986c906c706fbb7e44ec4bf1a553d9
AlanYachmenev <nhmurder@mail.ru>
2014-12-01 09:30:52 +0300

    HW1

---
+++ HW001/text/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\t And The Moon And The Stars And The World\n\t\t by Charles Bukowski\n\n\t Long walks at night--\n\t that�s what good for the soul:\n\t peeking into windows\n\t watching tired housewives\n\t trying to fight off\n\t their beer-maddened husbands.\n");
+
+
+    return 0;
+}
